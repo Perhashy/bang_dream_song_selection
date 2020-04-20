0ad2427057a166b90a324a71f76dad04edba5a3c
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-04-20 23:56:09 +0900

    追加

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,7 @@
 class Song < ActiveHash::Base
   self.data = [
-      {id: 1, name: "ときめきエクスペリエンス！", easy: "8", normal: "13", hard: "16", expert: "25", band_id: 1},
+      {id: 1, name: "ときめきエクスペリエンス！", easy: "8", normal: "13", hard: "16", expert: "25", band: "Poppin'Party"},
+      {id: 2, name: "キラキラだとか夢だとか〜Sing Girls〜", easy: "8", normal: "14", hard: "17", expert: "25", special: "26", band: "Poppin'Party"},
+
   ]
 end
