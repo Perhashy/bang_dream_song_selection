0f7fdd91bdad222eaed281bdddf099439b000b0a
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-04-21 16:59:32 +0900

    songs削除

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,5 @@
 class SongsController < ApplicationController
   def index
     @bands = Band.all
-    @songs = Song.all
   end
 end
--- app/models/song.rb
+++
@@ -1,7 +0,0 @@
-class Song < ActiveHash::Base
-  self.data = [
-      {id: 1, name: "ときめきエクスペリエンス！", easy: "8", normal: "13", hard: "16", expert: "25", band: "Poppin'Party"},
-      {id: 2, name: "キラキラだとか夢だとか〜Sing Girls〜", easy: "8", normal: "14", hard: "17", expert: "25", special: "26", band: "Poppin'Party"},
-
-  ]
-end
